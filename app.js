4577e9efe6e61691437e0b38e85f5ce8ef906cea
DanielAF07 <danar0609a@gmail.com>
2021-03-11 14:49:58 -0600

    playlists section done

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-$(function() {
+let $lists = [...document.getElementsByClassName('list')]
 
-    $(".list__playlists").mousewheel(function(event, delta) {
-    console.log(event.deltaX, event.deltaY, event.deltaFactor);
-       this.scrollLeft -= (delta * 2);
-       event.preventDefault();
-    });
- 
- });
+$lists.forEach(list => {
+    console.log(window.innerWidth);
+    let $listToScroll = list.querySelector('.list__playlists')
+    let relation = 0.5
+    list.querySelector('#left-arrow').onclick = function () {
+        $listToScroll.scroll({
+            left: $listToScroll.scrollLeft - (window.innerWidth * relation),
+            behavior: 'smooth'
+        })
+    }
+    list.querySelector('#right-arrow').onclick = function () {
+        $listToScroll.scroll({
+            left: $listToScroll.scrollLeft + (window.innerWidth * relation),
+            behavior: 'smooth'
+        })
+    }
+})
 
- console.log("object");
+$lists.forEach(list => {
+    list.querySelector('.list__playlists').addEventListener('scroll', function () {
+        let $scrollList = list.querySelector('.list__playlists')
+        let scrollLeft = $scrollList.scrollLeft
+        let scrollWidth = $scrollList.scrollWidth
+        let offsetWidth = $scrollList.offsetWidth
+        let contentWidth = scrollWidth - offsetWidth
+        console.log(scrollLeft)
+        if(scrollLeft > 0){
+            list.querySelector('#left-arrow').classList.toggle('arrow--active', true)
+        } else {
+            list.querySelector('#left-arrow').classList.toggle('arrow--active', false)
+        }
+        if(scrollLeft >= contentWidth){
+            list.querySelector('#right-arrow').classList.toggle('arrow--active', false)
+        } else {
+            list.querySelector('#right-arrow').classList.toggle('arrow--active', true)
+        }
+    })
+})
